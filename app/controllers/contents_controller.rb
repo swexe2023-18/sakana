84f66722ce8346ffd64f6db722ff75e7806b427f
Tanaka Ryusei <2011140040p@ed.fuk.kindai.ac.jp>
2023-12-13 09:12:35 +0000

    動画投稿機能

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,31 +8,71 @@ class ContentsController < ApplicationController
   end
 
   def create
-    file = params[:content][:image].read
-    video = params[:content][:mp3].read
-    content = Content.new(message: params[:content][:message], 
+        file = params[:content][:image]
+        video = params[:content][:mp3]
+        if file != nil
+          file = file.read
+        else
+          file = nil
+        end
+    
+        if video != nil
+          video = video.read
+        else
+          video = nil
+        end
+    
+    content = Content.new(message: params[:content][:message] , 
                           fish_name: params[:content][:fish_name],
                           size: params[:content][:size],
                           place_name: params[:content][:place_name],
                           image: file,
                           mp3: video)
     content.save
-    redirect_to content_path
+    redirect_to contents_path
   end
 
   def edit
+    @content = Content.find(params[:id])
+  end
+  
+  def update
+    file = params[:content][:image]
+    video = params[:content][:mp3]
+    if file != nil
+      file = file.read
+    else
+      file = nil
+    end
+    
+    if video != nil
+      video = video.read
+    else
+      video = nil
+    end
+    content = Content.find(params[:id])
+    content.update(message: params[:content][:message] , 
+                          fish_name: params[:content][:fish_name],
+                          size: params[:content][:size],
+                          place_name: params[:content][:place_name],
+                          image: file,
+                          mp3: video)
+    redirect_to contents_path
   end
 
   def destroy
+    content = Content.find(params[:id])
+    content.destroy
+    redirect_to contents_path
   end
   
   def get_image
     image = Content.find(params[:id])
-    send_data image.file, disposition: :inline, type: "image/png"
+    send_data image.image, disposition: :inline, type: "image/png"
   end
   
   def get_video
     video = Content.find(params[:id])
-    send_data video.file, disposition: :inline, type: "video/mp4"
+    send_data video.mp3, disposition: :inline, type: "video/mp4", controls: true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'top/main'
-  get 'top/login'
-  root 'top#main'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'contents#index'
-  resources :users, only: [:new, :create, :destroy]
-  resources :contents, only: [:index, :new, :create, :edit, :destroy]
-=======
     root 'contents#index'
     resources :users, only: [:new, :create, :destroy]
-    resources :contents, only: [:index, :new, :create, :edit, :destroy]
-    get 'get_image/:id', to: 'images#get_image'
-    get 'get_video/:id', to: 'videos#get_video'
+    resources :contents, only: [:index, :new, :create, :edit, :destroy, :edit, :update]
+    get 'get_image/:id', to: 'contents#get_image'
+    get 'get_video/:id', to: 'contents#get_video'
     get 'top/main'
     get 'top/login'
     post 'top/login'
     get 'top/logout'
     #root 'top#main'
->>>>>>> af9155aff06509564f3f864ef8119502ff1a9ee6
 end
